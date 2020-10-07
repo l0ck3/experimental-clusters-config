f17c29d0233f1f41a501adf897bff8d5383bd216
Yann IRBAH <yann.irbah@protonmail.com>
2020-10-07 15:19:02 +0200

    Clean flux mess

--- terraform/services.tf
+++ terraform/services.tf
@@ -60,50 +60,77 @@ resource "kubernetes_secret" "sealed-secrets-key-3" {
   type = "kubernetes.io/tls"
 }
 
-resource "kubernetes_namespace" "argo_cd" {
+resource "kubernetes_namespace" "flux" {
   metadata {
-    name = "argo-cd"
+    name = "flux"
   }
 }
 
-resource "helm_release" "argocd" {
-  name       = "argo-cd"
-  namespace  = kubernetes_namespace.argo_cd.metadata[0].name
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  version    = "2.8.0"
+resource "helm_release" "flux" {
+  name       = "flux"
+  namespace  = kubernetes_namespace.flux.metadata[0].name
+  repository = "https://charts.fluxcd.io"
+  chart      = "flux"
+  version    = "1.5.0"
 }
 
-resource "kubernetes_manifest" "argocd_application" {
-  provider = kubernetes-alpha
+resource "helm_release" "helm_operator" {
+  name       = "helm-operator"
+  namespace  = kubernetes_namespace.flux.metadata[0].name
+  repository = "https://charts.fluxcd.io"
+  chart      = "helm-operator"
+  version    = "1.2.0"
 
-  manifest = {
-    "apiVersion" = "argoproj.io/v1alpha1"
-    "kind" = "Application"
-    "metadata" = {
-      "name" = "cluster-services"
-      "namespace" = kubernetes_namespace.argo_cd.metadata[0].name
-    }
-    "spec" = {
-      "destination" = {
-        "server" =  "https://kubernetes.default.svc"
-        "namespace" = kubernetes_namespace.argo_cd.metadata[0].name
-      }
-      "project" = "default"
-      "source" = {
-        "directory" = {
-          "recurse" = true
-        }
-        "repoURL" = "https://github.com/l0ck3/experimental-clusters-config.git"
-        "targetRevision" = "HEAD"
-        "path" = "services"
-      }
-      "syncPolicy" = {
-        "automated" = {
-          "prune" = true
-          "selfHeal" = true
-        }
-      }
-    }
+  set {
+    name  = "helm.versions"
+    value = "v3"
   }
 }
+
+# resource "kubernetes_namespace" "argo_cd" {
+#   metadata {
+#     name = "argo-cd"
+#   }
+# }
+
+# resource "helm_release" "argocd" {
+#   name       = "argo-cd"
+#   namespace  = kubernetes_namespace.argo_cd.metadata[0].name
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argo-cd"
+#   version    = "2.8.0"
+# }
+
+# resource "kubernetes_manifest" "argocd_application" {
+#   provider = kubernetes-alpha
+
+#   manifest = {
+#     "apiVersion" = "argoproj.io/v1alpha1"
+#     "kind" = "Application"
+#     "metadata" = {
+#       "name" = "cluster-services"
+#       "namespace" = kubernetes_namespace.argo_cd.metadata[0].name
+#     }
+#     "spec" = {
+#       "destination" = {
+#         "server" =  "https://kubernetes.default.svc"
+#         "namespace" = kubernetes_namespace.argo_cd.metadata[0].name
+#       }
+#       "project" = "default"
+#       "source" = {
+#         "directory" = {
+#           "recurse" = true
+#         }
+#         "repoURL" = "https://github.com/l0ck3/experimental-clusters-config.git"
+#         "targetRevision" = "HEAD"
+#         "path" = "services"
+#       }
+#       "syncPolicy" = {
+#         "automated" = {
+#           "prune" = true
+#           "selfHeal" = true
+#         }
+#       }
+#     }
+#   }
+# }
